8b8fa8843ba5ce4663c18c444d68460cc070be06
WsrenMngtr <1447807246@qq.com>
2018-09-08 15:39:08 +0800

    原始算法完成

---
+++ getJson.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <string>
+#include <type_traits>
+
+// std::is_arithmetic<T>() == std::true_type()
+template <typename T>
+std::string getJsonArithmetic(const T& a) {
+	return std::to_string(a);
+}
+
+// char*, const char*, char* const, const char* const
+// char[], const char[]
+// std::string
+template <typename T>
+std::string getJsonStr(const T& str) {
+	return str;
+}
+
+// T* but not char*
+template <typename T>
+std::string getJsonPointer(const T& p) {
+	return 'p' + std::to_string((int)p);
+}
+
+// T[], const T[] but not char[]
+// vector, deque, list, forward_list, initlizer_list
+template <typename T>
+std::string getJsonList(const T& l) {
+	std::string str;
+	str.append("[");
+	for (const auto& element : l) {
+		str.append(getJson(element));
+		str.append(", ");
+	}
+	str.append("]");
+	return str;
+}
--- project.md
+++ project.md
@@ -2,7 +2,7 @@
 ## ԭʼ�㷨
 ```c++
 template <typename T>
-std::string getString(const T& t);
+std::string getJson(const T& t);
 
 // std::is_arithmetic<T>() == std::true_type()
 std::string str = std::to_string(a);
@@ -18,16 +18,17 @@ std::string str = 'p' + std::to_string((int)p);
 // T[], const T[] but not char[]
 // vector, deque, list, forward_list, initlizer_list
 std::string str;
-str.append("[")
+str.append("[");
 for (const T& element: array) {
-    str.append(getString(element));
+    str.append(getJson(element));
     str.append(", ");
 }
-str.append(]);
+str.append("]");
 
-//set,... 
+// set,... 
 
-//map,...
+// map,...
 
-
+// others
+std::string str = t.getJson
 ```
